6462c4e47142b47f878653f8121f84eb0fa8a505
Remi van Deursen <cryslo1994@gmail.com>
2016-10-06 17:48:53 +0200

    Added project

---
+++ Bootverhuur/Bootverhuur/DatabaseAcess.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Bootverhuur
+{
+    class DatabaseAcess
+    {
+        private static string connectionString = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=" + Application.StartupPath + "\\BotenverhuurDB.MDF;Integrated Security=True";
+        SqlConnection conn = new SqlConnection(connectionString);
+
+        public List<string> Columnrequest(string column, string table)
+        {
+            try
+            {
+                List<string> info = new List<string>();
+                SqlConnection sqlconn = new SqlConnection(connectionString);
+                sqlconn.Open();
+                string query = "SELECT " + column + " FROM " + table;
+                SqlCommand cmd = new SqlCommand(query, sqlconn);
+                using (SqlDataReader reader = cmd.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        info.Add(Convert.ToString(reader[column]));
+                    }
+                    sqlconn.Close();
+                    return info;
+                }
+            }
+            catch (Exception a)
+            {
+                return null;
+            }
+        }
+        public string RowRequest(string column, string table, string Name)
+        {
+            try
+            {
+                string info = "";
+                SqlConnection sqlconn = new SqlConnection(connectionString);
+                sqlconn.Open();
+                string query = "SELECT " + column + " FROM " + table + " WHERE Name = '" + Name + "'";
+                SqlCommand cmd = new SqlCommand(query, sqlconn);
+                using (SqlDataReader reader = cmd.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        info = Convert.ToString(reader[column]);
+                    }
+                    sqlconn.Close();
+                    return info;
+                }
+            }
+            catch (Exception a)
+            {
+                return null;
+            }
+        }
+
+        public void Add_BoatToDB(string ID, string Artikel)
+        {
+            try
+            {
+                SqlCommand cmd = new SqlCommand("INSERT INTO Artikelen (ArtikelID, Artikel) VALUES (@AID, @ART)");
+                
+                cmd.CommandType = CommandType.Text;
+                cmd.Connection = conn;
+                cmd.Parameters.AddWithValue("@AID", ID);
+                cmd.Parameters.AddWithValue("@ART", Artikel);
+
+                conn.Open();
+                cmd.ExecuteNonQuery();
+                conn.Close();
+            }
+            catch (Exception a)
+            {
+
+            }
+        }
+
+    }
+}
---
+++ Bootverhuur/Bootverhuur/Form1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Bootverhuur
+{
+    public partial class Form1 : Form
+    {
+        DatabaseAcess dbAcess;
+
+        public Form1()
+        {
+            InitializeComponent();
+            dbAcess = new DatabaseAcess();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            dbAcess.Add_BoatToDB("6", "Roeiboot");
+            List<string> listi = dbAcess.Columnrequest("Artikel", "Artikelen");
+            MessageBox.Show(listi[6]);
+        }
+    }
+}
